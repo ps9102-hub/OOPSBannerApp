ea71b4cc3aad8f1fc6d1303f09918fb210b1a32b
ps9102-hub <ps9102@srmist.edu.in>
2026-02-17 18:58:02 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,16 +2,55 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("   ***        ***      ******        ***** ");
-        System.out.println(" **   **    **   **    **    **     **     ");
-        System.out.println("**     **  **     **   **     **   **     ");
-        System.out.println("**     **  **     **   **    **     **  ");
-        System.out.println("**     **  **     **   ******        ***");
-        System.out.println("**     **  **     **   **              **  ");
-        System.out.println("**     **  **     **   **               **     ");
-        System.out.println(" **   **    **    **   **              **       ");
-        System.out.println("   ***        ***      **         *****    ");
+        // UC4: Store banner lines in String Array
+        String[] banner = new String[7];
 
+        banner[0] = String.join("   ",
+                "  OOOOO  ",
+                "  OOOOO  ",
+                "  PPPPP  ",
+                "  SSSSS  ");
+
+        banner[1] = String.join("   ",
+                " OO   OO ",
+                " OO   OO ",
+                " PP   PP ",
+                " SS      ");
+
+        banner[2] = String.join("   ",
+                " OO   OO ",
+                " OO   OO ",
+                " PPPPP   ",
+                "  SSSSS  ");
+
+        banner[3] = String.join("   ",
+                " OO   OO ",
+                " OO   OO ",
+                " PP      ",
+                "      SS ");
+
+        banner[4] = String.join("   ",
+                " OO   OO ",
+                " OO   OO ",
+                " PP      ",
+                " SS   SS ");
+
+        banner[5] = String.join("   ",
+                " OO   OO ",
+                " OO   OO ",
+                " PP      ",
+                " SS   SS ");
+
+        banner[6] = String.join("   ",
+                "  OOOOO  ",
+                "  OOOOO  ",
+                " PP      ",
+                "  SSSSS  ");
+
+        // Enhanced For Loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
 
