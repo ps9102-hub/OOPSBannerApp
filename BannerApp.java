b352447cd90bea9a4fd6ee1bdb0360e947254e64
ps9102-hub <ps9102@srmist.edu.in>
2026-02-24 08:40:13 +0530

    UC6: Refactored banner logic using static helper methods

---
+++ BannerApp.java
@@ -0,0 +1,56 @@
+public class BannerApp {
+
+    public static String[] getOPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
+    // Method to print a pattern
+    public static void printPattern(String[] pattern) {
+        for (String line : pattern) {
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Letter O:");
+        printPattern(getOPattern());
+
+        System.out.println("\nLetter P:");
+        printPattern(getPPattern());
+
+        System.out.println("\nLetter S:");
+        printPattern(getSPattern());
+    }
+}
