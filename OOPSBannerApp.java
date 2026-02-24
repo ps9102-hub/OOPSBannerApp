7ba750fa8aae9c11e84e6f5647ee1e09692b2054
ps9102-hub <ps9102@srmist.edu.in>
2026-02-24 09:09:08 +0530

    UC6: Refactored banner logic using static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,57 +1,56 @@
 public class OOPSBannerApp {
 
-    public static void main(String[] args) {
-
-        // UC5: Inline Array Initialization with String.join()
-        String[] banner = {
-                String.join("   ",
-                        "  OOOOO  ",
-                        "  OOOOO  ",
-                        "  PPPPP  ",
-                        "  SSSSS  "),
-
-                String.join("   ",
-                        " OO   OO ",
-                        " OO   OO ",
-                        " PP   PP ",
-                        " SS      "),
-
-                String.join("   ",
-                        " OO   OO ",
-                        " OO   OO ",
-                        " PPPPP   ",
-                        "  SSSSS  "),
-
-                String.join("   ",
-                        " OO   OO ",
-                        " OO   OO ",
-                        " PP      ",
-                        "      SS "),
-
-                String.join("   ",
-                        " OO   OO ",
-                        " OO   OO ",
-                        " PP      ",
-                        " SS   SS "),
-
-                String.join("   ",
-                        " OO   OO ",
-                        " OO   OO ",
-                        " PP      ",
-                        " SS   SS "),
-
-                String.join("   ",
-                        "  OOOOO  ",
-                        "  OOOOO  ",
-                        " PP      ",
-                        "  SSSSS  ")
+    public static String[] getOPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
         };
+    }
 
-        // Enhanced For Loop
-        for (String line : banner) {
+    public static String[] getPPattern() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
+    // Method to print a pattern
+    public static void printPattern(String[] pattern) {
+        for (String line : pattern) {
             System.out.println(line);
         }
     }
+
+    public static void main(String[] args) {
+        System.out.println("Letter O:");
+        printPattern(getOPattern());
+
+        System.out.println("\nLetter P:");
+        printPattern(getPPattern());
+
+        System.out.println("\nLetter S:");
+        printPattern(getSPattern());
+    }
 }
-
-
